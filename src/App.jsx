4fb0dd2efeb81a196feceb0254933da4bb754a25
leocsdev <leocsdev@gmail.com>
2023-07-13 23:44:54 +0800

    Fixed infinite scroll.

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,83 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import LaunchList from './LaunchList';
 import SearchForm from './components/SearchForm';
 import axios from 'axios';
-import { useState } from 'react';
 
 function App() {
   const [launches, setLaunches] = useState([]);
   const [filteredLaunches, setFilteredLaunches] = useState(launches);
   const [searchString, setSearchString] = useState('');
 
-  useEffect(() => {
-    axios
-      .get('https://api.spacexdata.com/v4/launches')
-      .then((res) => setLaunches(res.data));
-  }, []);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [page, setPage] = useState(1);
+  const [hasNextPage, setHasNextPage] = useState(false);
+
+  const fetchData = async () => {
+    setIsLoading(true);
+    setError(null);
+
+    try {
+      const response = await axios.post(
+        'https://api.spacexdata.com/v4/launches/query',
+        {
+          options: {
+            limit: 10,
+            page: page,
+          },
+        }
+      );
+
+      const data = await response.data;
+
+      // Add the newly fetched launches to the existing array
+      setLaunches((prevLaunches) => [...prevLaunches, ...data.docs]);
+      // Set to true if there is a next page
+      setHasNextPage(data.hasNextPage);
+      // Add 1 to the page number
+      setPage((prevPage) => prevPage + 1);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   useEffect(() => {
+    fetchData();
+  }, [isLoading]);
+
+  useEffect(() => {
+    setIsLoading(true);
+
     const newFilteredLaunches = launches.filter((launch) => {
       return launch.name.toLocaleLowerCase().includes(searchString);
     });
 
     setFilteredLaunches(newFilteredLaunches);
-  }, [launches, searchString]);
+
+    setIsLoading(false);
+  }, [launches, searchString, isLoading]);
 
   const onSearchChange = (event) => {
+    setIsLoading(true);
     const searchBoxString = event.target.value.toLocaleLowerCase();
 
     setSearchString(searchBoxString);
+    setIsLoading(false);
   };
 
+  console.log(filteredLaunches);
   return (
     <main className='bg-slate-100 p-8'>
       <SearchForm onChangeHandler={onSearchChange} />
-      <LaunchList launches={filteredLaunches} />
+      <LaunchList
+        launches={filteredLaunches}
+        dataLength={filteredLaunches.length}
+        next={fetchData}
+        hasMore={hasNextPage}
+        error={error}
+      />
     </main>
   );
 }
--- src/LaunchList.jsx
+++ src/LaunchList.jsx
@@ -1,26 +1,45 @@
-export default function LaunchList({ launches }) {
+import InfiniteScroll from 'react-infinite-scroll-component';
+
+export default function LaunchList({
+  launches,
+  dataLength,
+  next,
+  hasMore,
+  error,
+}) {
   return (
-    <ul>
-      {launches.map((launch) => (
-        <li
-          key={launch.name}
-          className='flex p-8 items-center max-w-4xl mx-auto bg-white rounded-md'
-        >
-          <img
-            src={launch.links.patch.small}
-            width='120px'
-            alt={launch.name}
-            className='pr-2 mr-4'
-          />
-          <div>
-            <h2 className='text-xl mb-2'>
-              {launch.flight_number}: {launch.name} (
-              {launch.date_local.slice(0, 4)})
-            </h2>
-            <p>{launch.details}</p>
-          </div>
-        </li>
-      ))}
-    </ul>
+    <>
+      <InfiniteScroll
+        dataLength={dataLength}
+        next={next}
+        hasMore={hasMore}
+        loader={<p>Loading...</p>}
+        endMessage={<p>no more data to load</p>}
+      >
+        <ul>
+          {launches.map((launch, index) => (
+            <li
+              key={`${launch.payloads[0]}-${index}`}
+              className='flex p-8 items-center max-w-4xl mx-auto bg-white rounded-md'
+            >
+              <img
+                src={launch.links.patch.small}
+                width='120px'
+                alt={launch.name}
+                className='pr-2 mr-4'
+              />
+              <div>
+                <h2 className='text-xl mb-2'>
+                  {launch.flight_number}: {launch.name} (
+                  {launch.date_local.slice(0, 4)})
+                </h2>
+                <p>{launch.details}</p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </InfiniteScroll>
+      {error && <p>Error: {error.message}</p>}
+    </>
   );
 }
